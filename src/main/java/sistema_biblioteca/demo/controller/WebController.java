bad57c4bf8d25974bf76602a4c02568c9dd7e356
Bryan Armando Lopez Pacherres <U22305277@utp.edu.pe>
2026-04-26 12:11:58 -0500

    feat: crear WebController para servir vistas de Thymeleaf

---
+++ src/main/java/sistema_biblioteca/demo/controller/WebController.java
@@ -0,0 +1,33 @@
+package sistema_biblioteca.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/news")
+    public String news() {
+        return "news";
+    }
+
+    @GetMapping("/schedules")
+    public String schedules() {
+        return "schedules";
+    }
+}
